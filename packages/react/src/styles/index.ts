96de83acdfe1723b4c59d85fc30bd56fcc909a26
Igor <delimaigor781@gmail.com>
2024-11-28 15:34:29 -0300

    Files updated

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@igniteuistudying/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
